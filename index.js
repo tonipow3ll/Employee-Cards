dc871d155a62383ae6ad3909aa8ecb7636854d8d
Toni Powell <tonipow3ll@gmail.com>
2021-02-19 18:21:04 -0500

    layout

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const Employee = require("./lib/employee")
+const Engineer = require("./lib/engineer")
+const Intern = require("./lib/intern")
+const Manager = require("./lib/manager")
 
 console.log("keep killin it")
