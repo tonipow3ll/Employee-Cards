dc1a667589470adfa0895a0f5db9aa3619fbb4ee
Toni Powell <tonipow3ll@gmail.com>
2021-02-20 15:30:38 -0500

    layout done

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
-const Employee = require("./lib/employee")
-const Engineer = require("./lib/engineer")
-const Intern = require("./lib/intern")
-const Manager = require("./lib/manager")
 
-console.log("keep killin it")
+const Employee = require("./lib/employee");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
+// const inquirer = require('inquirer');
+const fs = require('fs');
+
+const employee = new Employee();
+
+// inquirer
+//     .prompt([
+//     {
+//         type: 'input',
+//         name: 'name',
+//         message: 'Please enter employees name',
+//     },
+//     {
+//         type: 'input',
+//         name: 'employee-id',
+//         message: 'Please enter this employees ID',
+//     },
+//     {
+//         type: 'input',
+//         name: 'employee-id',
+//         message: 'Please enter this employees email',
+//     },
+//     {
+//         type: 'list',
+//         name: 'employee-type',
+//         message: 'What type of employee is this?',
+//         choices: ['Manager', 'Engineer', 'Intern']
+// }
+// ]).then((choices) => {
+//     // switch (choices) {
+//     //     case 'Manager':
+//     //         Employee = Manager()
+//     //         break;
+//     //     case 'Engineer':
+//     //         Employee = Engineer()
+//     //         break;
+//     //     case 'Intern':
+//     //         Employee = Intern();
+//     // }
+//     if (choices = 'Manager'){
+//        const manager = new Manager();
+//     } else if (choices = 'Engineer'){
+//        const engineer = new Engineer();
+//     } else if (choices = 'Intern'){
+//        const intern = new Intern();
+//     }
+
+// })
