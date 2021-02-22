d992d6a697b13c89406511053281b116615a1bc2
Toni Powell <tonipow3ll@gmail.com>
2021-02-22 16:13:36 -0500

    broke something

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ makeTeam = () => {
      .prompt([
      {
          type: 'input',
-         name: 'name',
+         name: 'managername',
          message: 'Please enter Managers name',
      },
      {
@@ -68,6 +68,9 @@ makeTeam = () => {
                 case "Intern":
                     newIntern();
                     break;
+                    case "Thats everyone!":
+                    createTeam();
+                    break;
             }
         })
     }
@@ -97,6 +100,7 @@ newEngineer = () => {
         message: 'Please enter this Engineers Github username',
     }
     ])
+    // can i change the below to promise.then ?? , promise.catch
     .then((answers) => {
         let engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
         theTeam.push(engineer);
@@ -143,12 +147,115 @@ newEngineer = () => {
     
 
 
-
+// upon app load, app will prompt user for "Manger Info" then execute based on inputs
 makeTeam()
 // code for creating HTML file 
-// const finalTeam = makeTeam(answers);
 
-    // fs.writeFile('index.html', finalTeam, (err) => {
-    //     err? console.log(err, "Something went wrong :(") : console.log('Team created - check *FILE LOCATION* to see the final product. ')
-    // })
+// BELOW CODE AND WRITE FILE CODE CURRENTLY BREAKS - APP QUITS BEFORE PROMPTS ARE ANSWERED 
+// const createFinalTeam = createTeam(answers);
+
+function createTeam(){
+ // INSERT HTML FILE HERE 
+ `<!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+     <title>Employee Cards</title>
+ </head>
+ <body>
+     <section class="hero is-small is-primary mb-3">
+         <div class="hero-body">
+           <h1 class="title has-text-centered">
+             My Team
+           </h1>
+           <!-- <p class="subtitle">
+             Small subtitle
+           </p> -->
+         </div>
+       </section>
+ 
+ 
+     <div class="columns has-text-centered">
+       <!-- MANAGER -->
+         <div class="column is-one-third">
+           
+ 
+             <div class="card is-one-third has-text-centered ">
+ 
+                 <div class="card-content">
+                   <div class="media">
+                     <div class="media-left">
+                     </div>
+                     <div class="media-content">
+                       <p class="title is-4">Manager</p>
+                       <p class="subtitle is-6">${answers.managername}</p>
+                     </div>
+                   </div>
+               
+                   <div class="content">
+                     <a target="_blank" href="mailto: ">Email</a>
+                     <p>Office Number:${answers.office} </p>
+                   </div>
+                 </div>
+               </div>
+           
+         </div>
+         <!-- ENGINEER -->
+         <div class="column is-one-third">
+           
+             <div class="card is-one-third has-text-centered ">
+                
+                 <div class="card-content">
+                   <div class="media">
+                     <div class="media-left">
+                     </div>
+                     <div class="media-content">
+                       <p class="title is-4">Engineer</p>
+                       <p class="subtitle is-6">Name</p>
+                     </div>
+                   </div>
+               
+                   <div class="content">
+                     <a target="_blank" href="mailto: ">Email</a>
+                     <p><a target="_blank" href="https://github.com/tonipow3ll">Github</a></p>
+                   </div>
+                 </div>
+               </div>
+         </div>
+ 
+         <!-- INTERN -->
+         <div class="column is-one-third">
+             <div class="card is-one-third has-text-centered ">
+                
+                 <div class="card-content">
+                   <div class="media">
+                     <div class="media-left">
+                     </div>
+                     <div class="media-content">
+                       <p class="title is-4">Intern</p>
+                       <p class="subtitle is-6">Name</p>
+                     </div>
+                   </div>
+               
+                   <div class="content">
+                     <a target="_blank" href="mailto: ">Email</a>
+                     <p>School</p>
+                     <br>
+                    
+                   </div>
+                 </div>
+               </div>
+         </div>
+     </div>
+     
+ </body>
+ </html>`
+}
+
+// fs.writeFile('index.html', createFinalTeam, (err) => {
+//     err ? console.log(err, "Something went wrong :(") : console.log('Team created - check *FILE LOCATION* to see the final product. ')
+//     })
 
