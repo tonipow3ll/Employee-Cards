4dfd0c83213a310fe318797cd396cb0764e790d5
Toni Powell <tonipow3ll@gmail.com>
2021-02-25 18:18:30 -0500

    are unit tests done?!

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -4,3 +4,30 @@ test("Can create a new engineer", () => {
     const e = new Engineer("Toni");
     expect(typeof (e)).toBe("object");
 });
+
+test("Can set name based on user input", () => {
+    const name = "Toni"
+    const e = new Engineer(name);
+    expect(e.name).toBe(name);
+});
+
+
+// test('can set employee ID', () => {
+//     const email = 'tonipow3ll@gmail.com';
+//     const e = new Engineer('Toni', 1 , email)});
+
+
+test('can set employee email', () => {
+    const email = 'tonipow3ll@gmail.com';
+    const e = new Engineer('Toni', 1 , email)
+    expect (e.email).toBe( email)
+});
+
+test('can set employee Github', () => {
+    const github = 'tonipow3ll';
+    const e = new Engineer('Toni', 7 , "tonipow3ll@gmail.com", github)
+    expect (e.github).toBe(github);
+});
+
+
+
--- tests/intern.test.js
+++ tests/intern.test.js
@@ -4,3 +4,21 @@ test("Can create a new intern", () => {
     const e = new Intern("Toni");
     expect(typeof (e)).toBe("object");
 });
+
+test("Can set name based on user input", () => {
+    const name = "Toni"
+    const e = new Intern(name);
+    expect(e.name).toBe(name);
+});
+
+test('can set employee email', () => {
+    const email = 'tonipow3ll@gmail.com';
+    const e = new Intern('Toni', 1 , email)
+    expect (e.email).toBe(email)
+});
+
+test('can set interns School', () => {
+    const school = 'tonipow3ll@gmail.com';
+    const e = new Intern('Toni', 7 , 'tonipow3ll@gmail.com', school)
+    expect (e.school).toBe(school)
+ });
