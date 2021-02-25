269a056f44ea13bfbf3fb9b6bf7ddc7896289a4f
Toni Powell <tonipow3ll@gmail.com>
2021-02-25 18:18:39 -0500

    are unit tests done?!

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -5,3 +5,19 @@ test("Can create a new employee", () => {
     expect(typeof (e)).toBe("object");
 });
 
+
+test("Can set name based on user input", () => {
+    const name = "Toni"
+    const e = new Employee(name);
+    expect(e.name).toBe(name);
+});
+
+test('can set employee email', () => {
+    const email = 'tonipow3ll@gmail.com';
+    const e = new Employee('Toni', 7 , email)
+    expect (e.email).toBe(email)
+});
+
+
+
+
--- tests/manager.test.js
+++ tests/manager.test.js
@@ -12,6 +12,19 @@ test("Can set name based on user input", () => {
 });
 
 
+test('can set employee email', () => {
+    const email = 'tonipow3ll@gmail.com';
+    const e = new Manager('Toni', 1 , email)
+    expect (e.email).toBe(email)
+});
+
+test('can set office number', () => {
+    const office = 1;
+    const e = new Manager('Toni', 1 , "tonipow3ll@gmail.com", office)
+    expect (e.office).toBe(office)
+});
+
+    
 // test("Can set Manager ID based on user input", () => {
 //     const ID = "7"
 //     const e = new Manager(ID);
