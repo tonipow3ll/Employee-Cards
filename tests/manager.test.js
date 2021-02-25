155c5fa4a655510603cf0a0d42ea99f035469669
Toni Powell <tonipow3ll@gmail.com>
2021-02-25 16:04:46 -0500

    tests were working until office/id/github

--- tests/manager.test.js
+++ tests/manager.test.js
@@ -4,3 +4,22 @@ test("Can create a new manager", () => {
     const e = new Manager("Toni");
     expect(typeof (e)).toBe("object");
 });
+
+test("Can set name based on user input", () => {
+    const name = "Toni"
+    const e = new Manager(name);
+    expect(e.name).toBe(name);
+});
+
+
+// test("Can set Manager ID based on user input", () => {
+//     const ID = "7"
+//     const e = new Manager(ID);
+//     expect(e.ID).toBe(ID);
+// });
+
+// test("Can set Manager Office Number based on user input", () => {
+//     const office = "10"
+//     const e = new Manager(office);
+//     expect(e.office).toBe(office);
+// });
